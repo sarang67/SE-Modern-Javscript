4979cff71e3b4bab38537928dd4b9c5ff648fdbb
sarang <sarang>
2021-11-19 17:06:34 +0530

    ******boolean in depth****** boolean literal ,boolean function for type conversion ,Boolean function constructor boolean type checking immutabilty and pass by value prototype methods
    ******Function in depth******
    function declaration , function expression ,short function/lambda function , arrow function after ES6 (2015)
    hoisting with function
    function rest parameter
    function return value,emplicit return
    function are object,first class function, means they are variable , means they are objects

--- src/index.js
+++ src/index.js
@@ -5,213 +5,264 @@ app.innerHTML = `
   <h1>Javascript Basic</h1>
 `;
 /*
-// Number in depth
-// Number methods
+// boolean in depth
+// boolean literal
 
-console.log(Number);
-console.dir(Number);
-console.log(Number.prototype);
+let isMarried = true;
+let hasChild = false;
 
-let myNO = 5;
+console.log(isMarried);
+console.log(hasChild);
 
-console.log((99.123).toFixed());
-console.log((99.567).toFixed());
-console.log(Number((99.567).toFixed()));
-console.log(parseFloat((99.567).toFixed()));
+// boolean function for type conversion
+let arg = "x";
+console.log(Boolean(arg));
+console.log(Boolean("sarang"));
+console.log(!!arg);
+console.log(Boolean({}));
+console.log(Boolean([]));
+console.log("---------------");
+console.log(Boolean());
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
 
-console.log((999.45).toPrecision(5));
-console.log((999.45).toPrecision(3));
+// Boolean function constructor
 
-console.log(new Number(50).valueOf(10));
+console.log(new Boolean(true));
+console.log(new Boolean(true).valueOf());
 
-*/
-//===============================================
+
+// boolean type checking
+console.log(typeof false);
+console.log(typeof true);
+console.log(typeof Boolean(true));
+console.log(Boolean(true) instanceof Boolean);
+console.log(new Boolean(true) instanceof Boolean);
+
+// immutabilty and pass by value
+console.log("---------------------------");
+let isMarried = true;
+let isMarriedRefrence = isMarried;
+
+console.log(isMarried);
+console.log(isMarriedRefrence);
+
+isMarried = false;
+console.log(isMarried);
+console.log(isMarriedRefrence);
+
+// prototype methods
+console.log("---------------------------");
+console.log(Boolean);
+console.dir(Boolean);
+console.dir(Boolean.prototype);
+
+console.log(true.toString());
+console.log(true);
+
+// Function in depth
+
+
 
 /*
-// string in depth
+//1 function declaration
+==========================
 
-// string creation
+function add() {
+  console.log("i am add function body");
+}
+// invoking the function, calling the funtion
+add();
+console.log(add);
+// parameter
+function add2(no1, no2) {
+  console.log(no1 + no2);
+}
+// argument
+add2(10, 20);
 
-// literal syntax
-let greet = "Hello Sarang";
-let pasta = "italian pasta";
-let areYouHappy = false;
+console.log("---------------------------");
 
-let one = 1;
-let two = 2;
+// 2 function expression (anonymous funtion)
+
+let addExpression = function () {
+  console.log("i am add function expression");
+};
+
+addExpression();
+
+let addExpression2 = function (no1, no2) {
+  console.log(no1 + no2);
+};
+
+addExpression2(50, 50);
+
+console.log(add2.name);
+console.log("---------------------------");
+
+//3 short function/lambda function , arrow function after ES6 (2015)
+
+//function add() {
+//  console.log("i am add function body");
+//}
+
+//function add2(no1, no2) {
+//  console.log(no1 + no2);
+//}
+
+
+let addArrow = () => {
+  console.log("i am add arrow function");
+};
+
+let addArrowParameter = (no1, no2) => {
+  console.log(no1 + no2);
+};
+
+let addArrowParameter2 = (no1, no2) => console.log(no1 + no2);
+
+addArrowParameter(10, 20);
+addArrowParameter2(50, 100);
+
+//4
+//function constructor
+
+var addfnco = new Function();
+console.log(addfnco);
+*/
+
+/*
+//hoisting with function
+==========================
+add(10, 20);
+console.log(add);
+console.log(addExp);
+console.log(addArrow);
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+let addExp = function (a, b) {
+  console.log(a + b);
+};
+
+let addArrow = (a, b) => console.log(a + b);
+*/
+
+/*
+// function rest parameter
+==========================
+function makePrice() {
+  console.log(arguments);
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total = total + arguments[i];
+  }
+  console.log(`Total price ${total}`);
+}
+
+makePrice(100, 200, 300, 400, 500);
+//makePrice(5000, 10000);
+
+console.log("------------------------------");
+
+function makePriceRest(a, b, ...param) {
+  console.log(a, param);
+
+  let total = 0;
+  for (let i = 0; i < param.length; i++) {
+    total = total + param[i];
+  }
+  console.log(`Total price ${total}`);
+}
+
+makePriceRest(100, 200, 300, 400, 500);
+*/
+
+/*
+// function return value
+// emplicit return
+
+function makePriceRest(...param) {
+  let total = 0;
+  for (let i = 0; i < param.length; i++) {
+    total = total + param[i];
+  }
+  // return undefined;
+}
+
+let result = makePriceRest(100, 200, 300, 400, 500);
+console.log(result);
+
+let result2 = makePriceRest(1, 2, 3, 4, 5);
+console.log(result2);
+*/
+
+/*
+  note: rest will always be a last parameter
+  function test(param1 , ...param){} --> valid
+   function test(param1 , ...param, param3){} --> invalid
+     function test( ...param, param1 , param3){} --> invalid
+
+*/
+
+// Functions are Objects:- first class function, means they are object
+
+/*
+In only JS , function are object, menas what you can do with object, you can do with function , you can assign a function in a variable, you can pass the function as a parameter, you can create function on the fly as a object literals.
+*/
+
+/*
+// function are object
+==========================
+// first class function, means they are variable , means they are objects
+
+let logparam = function (param) {
+  console.log(param);
+};
+
+logparam(1);
+logparam("sarang");
+logparam([]);
+logparam({});
+console.log("-----------------");
+
+logparam(function () {});
+logparam(function () {});
+// param = function {}
+// param = function(){}
+
+function add() {
+  console.log("i am a add fn");
+}
+
+logparam(add);
+logparam(() => {});
+
+
+let greet = function () {
+  console.log("hello");
+};
+
+greet();
+
 console.log(greet);
+console.dir(greet);
 
-console.log("My name is sarang jain");
-console.log("My name is sarang jain");
+greet.instructorName = "sarang jain";
 
-console.log("sarang");
+console.log(greet.instructorName);
 
-console.log('HI my name is "sarang jain"');
-console.log("HI my name is 'sarang jain'");
-console.log("hello 'sarang' \"jain\" ");
+greet.instructorName = "sarang kumar jain";
 
-console.log("my" + " " + pasta + " " + "and this is very yummy !!!");
-console.log("my " + pasta + " and this is very yummy !!!");
+console.log(greet.instructorName);
+// function call
+greet();
 
-// string back tick syntax  (es56)  ``
+// function using as a object
+greet.iamobj = "i am afn and i am obj as well";
 
-console.log(` Hello sarang i am in the back tick`);
-console.log(`my ${pasta} and this is very yummy !!!`);
-console.log(`my number is ${one}  ${two}`);
-
-console.log(` happy state  ${areYouHappy ? ":)" : ":("}`);
-console.log(`${2 + 2}`);
-
-console.log(`
-======================================
-name ===> 'sarang jain'
-age ===> 30,
-isMarried ===> yes i am married
-`);
-console.log("************************************");
-
-console.log(String(55));
-console.log(String("55"));
-console.log(String(true));
-console.log(String(55 + 11));
-
-console.log(String({ name: "sarang jain" }));
-console.log(String([1, 2, 3]));
-console.log(String(["Hi", "Hello", "Friends"]));
-
-console.log(55 + 11);
-console.log(55 + "11");
-console.log("11" + 55);
-console.log(String(11 + "11"));
-
-// function constructor (never used)
-let myName = new String("12");
-console.log(myName);
-console.log(myName.valueOf());
-
-*/
-//=============================================
-
-/*
-// we will check object
-
-let myInfo = {
-  name: "sarang",
-  age: 33,
-};
-
-// . operator
-
-// read
-console.log(myInfo.name);
-console.log(myInfo.age);
-
-// upade
-myInfo.name = "sarang jain";
-myInfo.age = 40;
-
-// add new property
-myInfo.city = "pune";
-
-console.log(myInfo);
-
-console.log("========================================");
-// [] member access operator
-
-let myInfo2 = {
-  name: "sarang",
-  age: 33,
-};
-
-// read
-console.log(myInfo2["name"]);
-console.log(myInfo2["age"]);
-
-// update
-myInfo2["name"] = "sarang jain";
-myInfo2["age"] = 40;
-
-// add a new property
-
-myInfo2["city"] = "pune";
-console.log(myInfo2);
-
-*/
-
-//====================================
-/*
-// string memory representation , and index
-
-console.log(new String("sarang jain"), "sarang jain");
-var myName = "sarang jain";
-
-console.log("sarang jain");
-console.log(myName[0]);
-console.log(myName["0"]);
-console.log(myName["1"]);
-console.log(myName.length);
-
-console.log(myName[myName.length - 1]);
-
-*/
-//================================================
-/*
-//immutability in string , pass by value
-
-let myName = "sarang";
-let refName = myName;
-
-myName += " jain"; //myName = myname + jain
-refName += " kumar jain";
-
-console.log(myName);
-console.log(refName);
-
-const immutablestr = "i will never be changed";
-const uppercase = immutablestr.toUpperCase();
-
-console.log(immutablestr);
-console.log(uppercase);
-*/
-
-//=======================================
-/*
-// type checking in string
-
-console.log(typeof "sarang");
-console.log(typeof "123");
-console.log(typeof new String("sarang jain"));
-
-console.log("sarang" instanceof String);
-console.log(String("sarang jain") instanceof String);
-
-*/
-//=================================================
-
-/*
-
-// string methods
-
-console.log(String);
-console.dir(String);
-console.log(String.prototype);
-
-console.log("sarang jain".indexOf("j"));
-console.log("sarang jain".indexOf("z"));
-
-console.log("sarang jain".includes("a"));
-
-console.log("sarangkumarjain".replace("sarang", "teacher"));
-
-console.log("sarangkumarjain".slice(0, 5));
-console.log("sarangkumarjain".slice(4, 5));
-console.log("sarangkumarjain".slice(4, 9));
-
-console.log("sarangkumarjain".slice(2, -1));
-console.log("sarangkumarjain".slice(2, -2));
-
-console.log("sarang~kumar~jain".split("n"));
-
-console.log("               4512 8795 1512 1212                 ".trim());
-
+console.dir(greet);
 */
