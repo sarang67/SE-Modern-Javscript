80b248904d6ec52400ca5b0fc1d34eb18d053606
sarang <sarang>
2021-12-12 21:32:20 +0530

    who is prototypes's constructor Employee function constructor using es5 approach ES6 approach using class pattern assignment solve and craete function for father information using ES5 assignment solve using ES6 approach class countryInformation ----> new assignment from class and using function constructor // class pattern // convert cart factory function to

--- src/index.js
+++ src/index.js
@@ -6,203 +6,233 @@ app.innerHTML = `
 `;
 
 /*
- Prototypes and .__proto__ , and prototype chain , and inheritance achiving by protptypl way
-===============================================================================================
-A) inheritance achiving by protptypl way
-=========================================
-Inheritance means one object can access the properties and method of another object, this terminology called inheritance. 
+// when ever you want to see who is function constructor of prototype
+console.log({}.constructor);
+console.log([].constructor);
 
-We can achieve inheritance in different language by classical way or other ways, but in JavaScript it is archived by only prototypal way. 
+function test() {}
+console.log(test.constructor);
 
-B) prototype and __proto__ ([[prototype]]) :-
-===========================================
-Every Object has prototype property , which keep the reference of another object.
-Every JavaScript object has a one prototype property (__proto__), which makes inheritance possible in JavaScript. 
+function Employee() {}
 
-C) prototype chain 
-=====================
-When a certain method or property is called , the search starts in the object itself first , if it is not found , theSearch moves on the object's prototype(__Proto__), its continue until the method or properties is found that is called prototype chain.
+console.log(new Employee());
 
-Note:-
-======
-Never ever use __proto__ property , it is not for us, it is used by JS internally.
-Every function has two prototype property (prototype) and (__prto__).
-Developer will always  use prototype property(prototype) property not prototype(__proto__) property , both has same purpose . 
-The Prototype property(prototype) in function is where we put methods and properties that we want in other object to inherit.
 
 */
 
 /*
-
-// __proto__ [[prototype]] property
-
-let myOBj = {};
-let myArray = [];
-let myFUnction = function () {};
-
-console.log(myOBj);
-console.log(myOBj.__proto__);
-
-console.log("=====================");
-
-console.log(myArray);
-console.log(myArray.__proto__);
-
-console.log("=====================");
-
-console.dir(myFUnction);
-console.dir(myFUnction.__proto__);
-
-
-====================================
-*/
-
-/*
-// protytype chain
-
-let obj_1 = {
-  prop1: "I am prop1",
-  // [[prototype]] / __proto__ : {}
-};
-
-let obj_2 = {
-  prop2: "I am prop2",
-  // [[prototype]] / __proto__ : {}
-};
-
-let obj_3 = {
-  prop3: "I am a prop3",
-  // [[prototype]] / __proto__ : {}
-};
-
-obj_1.__proto__ = obj_2;
-obj_2.__proto__ = obj_3;
-
-console.log(obj_1);
-
-console.log(obj_1.prop1);
-console.log(obj_1.prop2);
-console.log(obj_1.prop3);
-console.log(obj_1.prop4);
-*/
-
-/*
-=============================================================
-
-
-// why classes function constructor a,d blueprint are needed
-
-
-
-
-let emp1 = {
-  firstname: "sarang",
-  lastname: "jain",
-  age: 35,
-  getfullInformation() {
-    console.log(this.firstname + this.lastname + this.age);
-  },
-};
-
-let emp2 = {
-  firstname: "rrishi",
-  lastname: "shrivastava",
-  age: 28,
-  getfullInformation() {
-    console.log(this.firstname + this.lastname + this.age);
-  },
-};
-
-let emp3 = {
-  firstname: "rrishi",
-  lastname: "shrivastava",
-  age: 28,
-  getfullInformation() {
-    console.log(this.firstname + this.lastname + this.age);
-  },
-};
-
-console.log(emp1);
-console.log(emp2);
-console.log(emp3);
-
-console.log("=========================");
-
-function Employee(firstname, lastname, age) {
+// Employee function constructor using es5 approach
+============================================== 
+function Employeee(firstname, lastname) {
   this.firstname = firstname;
   this.lastname = lastname;
-  this.age = age;
-  this.getfullInformation = function () {
-    console.log(this.firstname + this.lastname + this.age);
-  };
 }
 
-let emp4 = new Employee("amar", "sjharma", 40);
-let emp5 = new Employee("abhsihe", "gupta", 45);
-let emp6 = new Employee("kanupriya", "jain", 20);
+Employeee.prototype.getFullName = function () {
+  return this.firstname + " " + this.lastname;
+};
 
-console.log(emp4);
-console.log(emp5);
-console.log(emp6);
+var sarang = new Employeee("sarang", "jain");
 
+console.log(sarang);
+sarang.getFullName();
 
+var kapil = new Employeee("kapil", "gandhi");
 
-=================================
+console.log(kapil);
+kapil.getFullName();
 
 */
 
+/*
+// this is the ES6 approach using class pattern
+===========================
+
+class Employee {
+  constructor(fn, ln, a) {
+    this.firstname = fn;
+    this.lastName = ln;
+    this.age = a;
+  }
+
+  getAge() {
+    console.log(this.age);
+  }
+
+  getfullName() {
+    console.log(this.name + this.age);
+  }
+}
+
+console.log(typeof Employee);
+
+let obj1 = new Employee("rishi", "shrivastava", "45");
+console.log(obj1);
+*/
+
+// function constructor assignment solve and craete function for father information using ES5
 
 /*
-=================================
+ FamilyIformation {
+   --> name
+   -->lastName
+   ---> age
+   ---> fathername
+   --> motherName
+   --> fatherInfo : {
+       fFathertname 
+       fMothername
+   }
+   motherInfo :{
+     mfathername
+     mMotherName
+   }
 
+   getfatherInformation()
+   getmotherInformati()
+}
+*/
 
-// how we write the classes
-
-function Emplyee(fn, ln, ag) {
-  this.firstname = fn;
-  this.lastname = ln;
-  this.age = ag;
-  this.getname = function () {
-    console.log(this.firstname);
+/*
+function FamililyInformation(
+  fname,
+  lname,
+  age,
+  fNam,
+  mName,
+  fFName,
+  fMname,
+  mFname,
+  mMName
+) {
+  this.name = fname;
+  this.lastName = lname;
+  this.age = age;
+  this.fatherName = fNam;
+  this.motherName = mName;
+  this.fatherInformation = {
+    fFName,
+    fMname,
   };
-  this.getfulEmployeeInformation = function () {
-    console.log(this.firstname + "" + this.lastname + this.age);
+  this.motherInformation = {
+    mFname,
+    mMName,
   };
 }
 
-let emp1 = new Emplyee("sarang", "jain", 33);
-console.log(emp1);
+FamililyInformation.prototype.getMotherInformation = function () {
+  const { mFname, mMName } = this.motherInformation;
+  console.log(mFname + "" + mMName);
+};
+
+let mySister = new FamililyInformation(
+  "shalini",
+  "jain",
+  30,
+  "maya jain",
+  "p k jain ",
+  "k.c jain",
+  "dadi jain",
+  "abc jain",
+  "xyz jain"
+);
+
+// mySister.__proto__ = FamililyInformation.prototype
+
+console.log(mySister);
+mySister.getMotherInformation();
 
 
+ // assignment solve using ES6 approach class
+ ===========================================
 
-// problems in this class we never have to write method inside the class , we have to write inside the parent prototype
+class FamililyInformation2 {
+  constructor(fname, lname, age, fNam, mName, fFName, fMname, mFname, mMName) {
+    this.name = fname;
+    this.lastName = lname;
+    this.age = age;
+    this.fatherName = fNam;
+    this.motherName = mName;
+    this.fatherInformation = {
+      fFName,
+      fMname,
+    };
+    this.motherInformation = {
+      mFname,
+      mMName,
+    };
+  }
 
-function Emplyee(fn, ln, ag) {
-  this.firstname = fn;
-  this.lastname = ln;
-  this.age = ag;
+  getMotherInformation() {
+    const { mFname, mMName } = this.motherInformation;
+    console.log(mFname + "" + mMName);
+  }
 }
 
-// this is  recommneded way  for method
-Emplyee.prototype.getname = function () {
-  console.log(this.firstname);
-};
+let mySister2 = new FamililyInformation(
+  "shalini",
+  "jain",
+  30,
+  "maya jain",
+  "p k jain ",
+  "k.c jain",
+  "dadi jain",
+  "abc jain",
+  "xyz jain"
+);
 
-Emplyee.prototype.getfulEmployeeInformation = function () {
-  console.log(this.firstname + "" + this.lastname + this.age);
-};
+// mySister.__proto__ = FamililyInformation.prototype
 
-console.dir(Emplyee);
+console.log(mySister2);
+mySister2.getMotherInformation();
 
-let emp1 = new Emplyee("sarang", "jain", 33);
-// emp1.__proto__ = Emplyee.prototype  ---> behind the scene JS is doing for us
-console.log(emp1);
-emp1.getname();
-emp1.getfulEmployeeInformation();
+class Classname {
+  constructor() {}
 
-let emp2 = new Emplyee("rishi", "shrivastava", 33);
-// emp2.__proto__ = Employee.prototype ---> behind the scene JS is doing for us
-console.log(emp2);
+  m1() {}
+  m2() {}
+}
+*/
 
+/*
+// countryInformation ----> new assignment from class and using function constructor
+==================================================================================
+--> city
+                      --> state
+                      --> currency
+                      --> prime minsiter
+                      --> language : []
+                      --> counryName
+                      ---> nighbourscountrty : []
+                      -->getCountryName
+                      --> getneghburs countryname                        
 
+*/
+
+/*
+
+// class pattern // convert cart factory function to 
+
+class CreateCart {
+  constructor(items = []) {
+    this.items = Object.freeze(items);
+    this.#test = "sarang jain";
+  }
+
+  add(item) {
+    const state = [...this.items, item];
+    this.items = Object.freeze(state);
+  }
+
+  remove(id) {
+    const state = this.items.filter((item) => item.id !== id);
+    this.items = Object.freeze(state);
+  }
+}
+
+let minicart = new CreateCart([{ id: "🌭", name: "hot dog", price: 399 }]);
+
+minicart.add({ id: "🍕", name: "cheese pizaa", price: 1000 });
+
+console.log(minicart);
 */
