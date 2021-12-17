ab6ec00b8afe9f30ce6297207f15782a29c2547d
sarang <sarang>
2021-12-17 09:15:28 +0530

    Doubt clear Classes and Members Private and Static Class Members Setters and Getters Class Inheritance via ‘extends’

--- src/index.js
+++ src/index.js
@@ -6,233 +6,203 @@ app.innerHTML = `
 `;
 
 /*
-// when ever you want to see who is function constructor of prototype
-console.log({}.constructor);
-console.log([].constructor);
+// private property and static class member
 
-function test() {}
-console.log(test.constructor);
+class Cart {
+  // declare the members prposal of the property
+  #items; // private property ---> can be used inside the class , not outside the class
+  property1;
 
-function Employee() {}
-
-console.log(new Employee());
-
-
-*/
-
-/*
-// Employee function constructor using es5 approach
-============================================== 
-function Employeee(firstname, lastname) {
-  this.firstname = firstname;
-  this.lastname = lastname;
-}
-
-Employeee.prototype.getFullName = function () {
-  return this.firstname + " " + this.lastname;
-};
-
-var sarang = new Employeee("sarang", "jain");
-
-console.log(sarang);
-sarang.getFullName();
-
-var kapil = new Employeee("kapil", "gandhi");
-
-console.log(kapil);
-kapil.getFullName();
-
-*/
-
-/*
-// this is the ES6 approach using class pattern
-===========================
-
-class Employee {
-  constructor(fn, ln, a) {
-    this.firstname = fn;
-    this.lastName = ln;
-    this.age = a;
+  // static property or utility property
+  static name = "cart class";
+  static methodfn() {
+    console.log(
+      "I am a static function , i can be used directly from the class name"
+    );
   }
 
-  getAge() {
-    console.log(this.age);
-  }
-
-  getfullName() {
-    console.log(this.name + this.age);
-  }
-}
-
-console.log(typeof Employee);
-
-let obj1 = new Employee("rishi", "shrivastava", "45");
-console.log(obj1);
-*/
-
-// function constructor assignment solve and craete function for father information using ES5
-
-/*
- FamilyIformation {
-   --> name
-   -->lastName
-   ---> age
-   ---> fathername
-   --> motherName
-   --> fatherInfo : {
-       fFathertname 
-       fMothername
-   }
-   motherInfo :{
-     mfathername
-     mMotherName
-   }
-
-   getfatherInformation()
-   getmotherInformati()
-}
-*/
-
-/*
-function FamililyInformation(
-  fname,
-  lname,
-  age,
-  fNam,
-  mName,
-  fFName,
-  fMname,
-  mFname,
-  mMName
-) {
-  this.name = fname;
-  this.lastName = lname;
-  this.age = age;
-  this.fatherName = fNam;
-  this.motherName = mName;
-  this.fatherInformation = {
-    fFName,
-    fMname,
-  };
-  this.motherInformation = {
-    mFname,
-    mMName,
-  };
-}
-
-FamililyInformation.prototype.getMotherInformation = function () {
-  const { mFname, mMName } = this.motherInformation;
-  console.log(mFname + "" + mMName);
-};
-
-let mySister = new FamililyInformation(
-  "shalini",
-  "jain",
-  30,
-  "maya jain",
-  "p k jain ",
-  "k.c jain",
-  "dadi jain",
-  "abc jain",
-  "xyz jain"
-);
-
-// mySister.__proto__ = FamililyInformation.prototype
-
-console.log(mySister);
-mySister.getMotherInformation();
-
-
- // assignment solve using ES6 approach class
- ===========================================
-
-class FamililyInformation2 {
-  constructor(fname, lname, age, fNam, mName, fFName, fMname, mFname, mMName) {
-    this.name = fname;
-    this.lastName = lname;
-    this.age = age;
-    this.fatherName = fNam;
-    this.motherName = mName;
-    this.fatherInformation = {
-      fFName,
-      fMname,
-    };
-    this.motherInformation = {
-      mFname,
-      mMName,
-    };
-  }
-
-  getMotherInformation() {
-    const { mFname, mMName } = this.motherInformation;
-    console.log(mFname + "" + mMName);
-  }
-}
-
-let mySister2 = new FamililyInformation(
-  "shalini",
-  "jain",
-  30,
-  "maya jain",
-  "p k jain ",
-  "k.c jain",
-  "dadi jain",
-  "abc jain",
-  "xyz jain"
-);
-
-// mySister.__proto__ = FamililyInformation.prototype
-
-console.log(mySister2);
-mySister2.getMotherInformation();
-
-class Classname {
-  constructor() {}
-
-  m1() {}
-  m2() {}
-}
-*/
-
-/*
-// countryInformation ----> new assignment from class and using function constructor
-==================================================================================
---> city
-                      --> state
-                      --> currency
-                      --> prime minsiter
-                      --> language : []
-                      --> counryName
-                      ---> nighbourscountrty : []
-                      -->getCountryName
-                      --> getneghburs countryname                        
-
-*/
-
-/*
-
-// class pattern // convert cart factory function to 
-
-class CreateCart {
   constructor(items = []) {
-    this.items = Object.freeze(items);
-    this.#test = "sarang jain";
+    // inilization
+    this.#items = Object.freeze(items);
+    this.property1 = "inilized";
   }
 
   add(item) {
-    const state = [...this.items, item];
-    this.items = Object.freeze(state);
+    const state = [...this.#items, item];
+    this.#items = Object.freeze(state);
   }
-
   remove(id) {
-    const state = this.items.filter((item) => item.id !== id);
-    this.items = Object.freeze(state);
+    const state = this.#items.filter((item) => item.id !== id);
+    this.#items = Object.freeze(state);
   }
 }
 
-let minicart = new CreateCart([{ id: "🌭", name: "hot dog", price: 399 }]);
+const cart = new Cart();
+const hotdog = { id: "🌭", name: "Posh Dog", price: 399 };
+cart.add(hotdog);
 
-minicart.add({ id: "🍕", name: "cheese pizaa", price: 1000 });
+console.log(cart);
+//console.log(cart.#items);
 
-console.log(minicart);
+console.log(Cart.name);
+console.log(Cart.methodfn());
+
+console.dir(Cart);
+
+// class MathUtility
+
+class MathUtility {
+  static pi = 3.14;
+  static inch = "some unit";
+  static cm = "some unint";
+}
+
+console.dir(MathUtility);
+*/
+
+/*
+/// setter and getter
+=================
+// getter setter
+class Cart {
+  #items;
+  // static property or utility property
+
+  constructor(items = []) {
+    this.value = items; //  set value(items)
+  }
+
+  set value(items) {
+    this.#items = Object.freeze(items);
+  }
+
+  get value() {
+    return this.#items;
+  }
+
+  add(item) {
+    const state = [...this.value, item]; // get value()
+    this.value = Object.freeze(state); //set value(items)
+  }
+
+  remove(id) {
+    const state = this.value.filter((item) => item.id !== id); // // get value()
+    this.value = Object.freeze(state); // set value(items)
+  }
+
+  get count() {
+    return this.#items.length;
+  }
+
+  dispalycount() {
+    console.log(this.#items.length);
+  }
+}
+
+const cart = new Cart();
+const hotdog = { id: "🌭", name: "Posh Dog", price: 399 };
+cart.add(hotdog);
+
+console.log(cart.dispalycount());
+
+console.log(cart.value); // get value();
+console.log(cart.count); // get count()
+============================================
+*/
+
+/*
+
+// Inheritance : - one object can access the property of another object
+class Cart {
+  items;
+  constructor(items = []) {
+    this.value = items; //  set value(items)
+  }
+
+  set value(items) {
+    this.items = Object.freeze(items);
+  }
+
+  get value() {
+    return this.items;
+  }
+
+  add(item) {
+    const state = [...this.value, item]; // get value()
+    this.value = Object.freeze(state); //set value(items)
+  }
+
+  remove(id) {
+    const state = this.value.filter((item) => item.id !== id); // // get value()
+    this.value = Object.freeze(state); // set value(items)
+  }
+}
+
+const cart = new Cart();
+
+class Product {
+  id;
+  name;
+  price;
+  constructor(id, name, price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+  get displayName() {
+    return `${this.id} ${this.name}`;
+  }
+}
+
+// food class inheriting Product class
+class Food extends Product {
+  // i will handle extra array
+  constructor(id, name, price, extra) {
+    // yu are calling parent class constructor
+    super(id, name, price);
+    this.extra = extra;
+  }
+}
+
+// Drink class inheriting product class
+
+class Drink extends Product {
+  // i will handle extra string
+  constructor(id, name, price, extra) {
+    super(id, name, price);
+    this.extra = extra;
+  }
+}
+
+//const hotdog = { id: "🌭", name: "Posh Dog", price: 399 };
+//const hotdog = new Product("🌭", "Posh Dog", 399, ["pickles"]);
+//const burger = new Product("🍔", "super burger", 449, ["custored", "onion"]);
+
+const hotdog = new Food("🌭", "Posh Dog", 399, ["pickles"]);
+const burger = new Food("🍔", "super burger", 449, ["custored", "onion"]);
+
+//const smallDrink = new Product("🥂", "small slurp", 199, "small");
+//const mediumDrink = new Product("🍷", "medium slurp", 199, "medium");
+//const largeDrink = new Product("🍾", "large slurp", 199, "large");
+
+const smallDrink = new Drink("🥂", "small slurp", 199, "small");
+const mediumDrink = new Drink("🍷", "medium slurp", 199, "medium");
+const largeDrink = new Drink("🍾", "large slurp", 199, "large");
+
+cart.add(hotdog);
+cart.add(burger);
+cart.add(smallDrink);
+cart.add(mediumDrink);
+cart.add(largeDrink);
+console.log(cart);
+
+console.log(hotdog);
+
+console.log(hotdog instanceof Food);
+console.log(hotdog instanceof Product);
+
+//
+console.log([] instanceof Array);
+console.log([] instanceof Object);
+
 */
