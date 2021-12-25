5e61e02821389ff36bdb5736f66d7ae21ddca08f
sarang <sarang>
2021-12-25 23:18:27 +0530

    *****Querying and Traversing the DOM ******** Finding Parent Elements Finding Sibling Elements
    ***Attributes, Styles and Classes *******
    Element Properties versus HTML Attributes
    Setting and Getting HTML Attributes
    Setting and Getting Inline Styles
    Setting and Getting Classes
    
    *******Events and Event Listeners*******
    Event introduction , what is event how they emit , how to capture them.

--- src/index.js
+++ src/index.js
@@ -3,104 +3,60 @@ import "../assets/css/style.css";
 const app = document.getElementById("app");
 app.innerHTML = `
   <h1>Javascript Basic</h1>
+   <!-- <button style="padding:20px; font-size:20; color:red;border-left:2px solid green"> Click Me </button> -->
+   <button> Click Me </button>
+   `;
+
+/***
+      * The parent node property is read only property which returns
+      us the name of the parent node of the selected node as a node
+      object. The Node object represents a single node in the 
+      document tree and a node can be an element node, 
+      text node or more.
+
+      The parent element is read only property which returns the parent 
+      element of the selected element.The element object represents an 
+      HTML element, like P, DIV, etc.
+
+      // event emitting --> browser will do continously
+      // event capturing ---> if you want to takse some action on perticular event then capture it
+ 
+* */
+
+/*
+// finding parent element
+
+app.innerHTML = `
+  <h1>Javascript Basic</h1>
+  <div class="item"></div>
 `;
+
+console.log(app);
+const div = document.querySelector(".item");
+console.log(div);
+
+console.log(div.parentNode);
+console.log(div.parentNode.parentNode.parentNode);
+
+console.log("=================");
+console.log(div.parentElement);
+console.log(div.parentElement.parentElement);
+console.log(div.parentElement.parentElement.parentElement);
+
+console.log("====================");
+console.log(div.closest("#app"));
+
+==============================
+*/
+
+/*
 
 // ============== Querying in the DOM ======================
-/*
-// Querying DOM Nodes (HTMLCollections)getElementById : this will give live data
+// finding the sibling
 
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <ul id="ulElement" class="ul-list"></ul>
-`;
-
-const ulElem = document.getElementById("ulElement");
-
-const names = ["sarang", "manish", "kk", "amar"];
-
-names.forEach((name) => {
-  const liItem = document.createElement("li");
-  liItem.textContent = name;
-  liItem.className = "li-list";
-  ulElem.append(liItem);
-});
-
-console.dir(ulElem);
-
-//getElementsByClassName :  HTMLCollection live collection
-const listByClassname = document.getElementsByClassName("li-list");
-console.log(listByClassname);
-
-// getElementBYTagname :  HTMLCollection live collection
-
-const listByTagname = document.getElementsByTagName("li");
-console.log(listByTagname);
-
-const newFriend = `<li class="li-list">vasu</li>`;
-ulElem.innerHTML += newFriend;
-
-console.log("===================");
-console.log(listByTagname);
-console.log(listByClassname);
-
-// ======================
-*/
-
-/*
-
-// Querying DOM Nodes (NodeLists)---> this gives snapshot
-
-app.innerHTML = `
-  <h1>JavaScript DOM</h1>
-  <ul id="ulElement" class="ul-list"></ul>
-`;
-
-// const ulElem = document.getElementById("ulElement");
-
-const ulElem = document.querySelector("ul");
-console.log(ulElem);
-//#ulElement {}
-// .ulElement {}
-// ul li
-
-const names = ["sarang", "manish", "kk", "amar"];
-
-names.forEach((name) => {
-  const liItem = document.createElement("li");
-  liItem.textContent = name;
-  liItem.className = "li-list";
-  ulElem.append(liItem);
-});
-
-console.dir(ulElem);
-
-//querySelectorAll :nodeList
-
-const ulElemAll = document.querySelectorAll("li");
-console.log(ulElemAll);
-
-const ulElemt = `<li class="li-list">rishi</li>`;
-ulElem.innerHTML += ulElemt;
-
-console.log("===================");
-console.log(ulElemAll);
-
-const ulElemAllAgain = document.querySelectorAll("li");
-console.log(ulElemAllAgain);
-
-*/
-
-//=================
-
-// Looping over DOM Elements
-
-/*
-import "../assets/css/style.css";
-
-const app = document.getElementById("app");
-app.innerHTML = `
-  <h1>JavaScript DOM</h1>
-  <ul id="ulElem" class="ul-list">
+  <ul id="ulElement" class="ul-list">
     <li class="li-list">Sarng</li>
     <li class="li-list">Saurabh</li>
     <li class="li-list">manish</li>
@@ -108,64 +64,147 @@ app.innerHTML = `
  </ul>
 `;
 
-const ulLiElementList = document.querySelectorAll(".li-list");
-console.log(ulLiElementList);
+const listItem = document.querySelector("#ulElement li");
+console.log(listItem);
 
-console.log("===for loop====");
-for (let i = 0; i < ulLiElementList.length; i++) {
-  console.log(ulLiElementList[i]);
-}
+// next sibling
+console.log(listItem.nextSibling);
+console.log(listItem.nextElementSibling);
 
-console.log("===for..of====");
-for (let item of ulLiElementList) {
-  console.log(item);
-}
+// previous sibling
 
-console.log("===for each====");
+console.log(listItem.previousSibling);
+console.log(listItem.previousElementSibling);
 
-ulLiElementList.forEach((item) => console.log(item));
-
-console.log("===spread====");
-
-[...ulLiElementList].forEach((item) => console.log(item));
-
-console.log("===array from====");
-
-Array.from(ulLiElementList).forEach((item) => console.log(item));
+**************************************************************
 */
 
-// Finding Child Elements
-
 /*
-import "../assets/css/style.css";
-
-const app = document.getElementById("app");
+// ******** Html attributes v/s javascript dom properties**************
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <ul id="ulElem" class="ul-list">
-    <li class="li-list">Sarng</li>
-    <li class="li-list">Saurabh</li>
-    <li class="li-list">manish</li>
-    <li class="li-list">kk</li>
- </ul>
-`;
+  <input type="text" />
+  `;
 
-const ulElemet = document.querySelector("#ulElem");
-const liElemnts = ulElemet.querySelectorAll(".li-list");
+const input = document.querySelector("input");
+console.log(input);
 
-console.log(ulElemet);
-console.log(liElemnts); // nodeList
+console.dir(input);
 
-console.log("===childre/childNodes==========");
-console.log(ulElemet.children); // HtmlCollection
-console.log(ulElemet.childNodes); // nodes with whitespace
+// getting properties
+console.dir(input.type);
+console.log(input.value);
 
-console.log("===first/last==========");
-console.log(ulElemet.firstChild);
-console.log(ulElemet.firstElementChild);
+//setting properties
+input.value = 50;
+// mirroring attributes and properties
+input.setAttribute("value", 50);
+//input.type = "email";
 
-console.log(ulElemet.lastChild);
-console.log(ulElemet.lastElementChild);
+*/
+
+/*
+=========================
+  <h1>Javascript Basic</h1>
+   <!-- <button style="padding:20px; font-size:20; color:red;border-left:2px solid green"> Click Me </button> -->
+   <button> Click Me </button>
+   `;
+
+// ******** setting and getting styles**************
+
+const btn = document.querySelector("button");
+console.dir(btn);
+console.dir(btn.style);
+console.log(btn.style.cssText);
+
+const myStyle =
+  "padding:20px; font-size:20; color:red;border-left:2px solid green";
+
+btn.style.cssText = myStyle;
+
+btn.style.padding = "20px";
+btn.style.fontSize = "20px";
+btn.style.color = "red";
+btn.style["border-left"] = "2px solid green";
+
+document.querySelector("h1").style.color = "red";
+*/
+
+/*
+========================
+
+/*
+
+================================================
+// setting and getting class
+
+
+app.innerHTML = `
+  <h1>Javascript Basic</h1>
+   <!-- <button style="padding:20px; font-size:20; color:red;border-left:2px solid green"> Click Me </button> -->
+   <button class="one  two"> Click Me </button>
+   `;
+
+
+
+
+const btn = document.querySelector("button");
+console.dir(btn);
+console.dir(btn.className);
+console.dir(btn.classList);
+
+// old way set class
+btn.className += " three";
+
+btn.classList.add("four");
+btn.classList.remove("one");
+
+// toggle behaviour  true --> false --> true
+setTimeout(() => {
+  btn.classList.toggle("four"); // if avaibale --> remove
+}, 8000); // 8 sec
+
+setTimeout(() => {
+  btn.classList.toggle("four"); // if avaibale --> add
+}, 10000);  // 10 sec
+
+console.dir(btn.className);
+
+btn.classList.add("nine", "ten");
+btn.classList.add("two", "three");
+
+
+*/
+
+/*
+=========================================
+
+// *****************Event ****************\
+
+// event emitting --> browser will do continously
+// event capturing ---> if you want to takse some action on perticular event then capture it
+
+const btn = document.querySelector("button");
+const h1 = document.querySelector("h1");
+console.dir(btn);
+
+// event capturing
+btn.onmouseenter = function () {
+  console.log("I am entering");
+};
+
+btn.onmouseleave = function () {
+  console.log("I am leaving");
+};
+
+h1.onclick = function () {
+  console.log("I am also clicked");
+};
+
+h1.oncopy = function () {
+  console.log("I am copied");
+};
+
 
 
 */
